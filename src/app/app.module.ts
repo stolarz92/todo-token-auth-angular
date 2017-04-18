9f6ccf8c458905bee92c04cc9a949a089f2f7c4a
Radoslaw Stolarski <stolarz92@gmail.com>
2017-04-18 20:56:43 +0200

    Home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { Angular2TokenService } from 'angular2-token';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterializeModule } from 'angular2-materialize';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
