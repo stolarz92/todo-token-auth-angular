b29b8a6b5da5950fc96783a504ce9725109109db
Radoslaw Stolarski <stolarz92@gmail.com>
2017-04-22 22:19:39 +0200

    Update questions component

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+// Importa nosso Service
+import { TodoService } from './shared/todo.service';
+// Importa nosso model
+import {Todo} from './shared/todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.sass']
+  styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
 
-  constructor() { }
+  private todos: Todo[] = [];
 
+  constructor(private todoService: TodoService) { }
+
+  // Pega a listagem de Todos ao iniciar
   ngOnInit() {
+    this.todoService.getTodos()
+      .subscribe(data => this.todos = data);
+  }
+
+  // Apaga a questão
+  deleteTodo(todos) {
+    if (confirm('Você tem certeza que quer deletar a todos ' + todos.title + '?')) {
+      var index = this.todos.indexOf(todos);
+      this.todos.splice(index, 1);
+
+      this.todoService.deleteTodo(todos.id)
+        .subscribe(null);
+    }
   }
 
 }
